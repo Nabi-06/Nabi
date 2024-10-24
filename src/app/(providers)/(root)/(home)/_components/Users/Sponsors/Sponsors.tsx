faee4aef105d738bd4b58e4fe29a726a8376f62d
심우섭 <sybiyong2@gmail.com>
2024-10-24 16:16:33 +0900

    refactor: sponsor

--- src/app/(providers)/(root)/(home)/_components/Users/Sponsors/Sponsors.tsx
+++ src/app/(providers)/(root)/(home)/_components/Users/Sponsors/Sponsors.tsx
@@ -14,16 +14,19 @@ function Sponsors() {
 
   if (isLoading)
     return (
-      <ul className="grid grid-cols-1 grid-rows-5 gap-y-2">
-        {Array(5)
-          .fill(null)
-          .map((_, index) => (
-            <li key={index} className="flex gap-x-4 items-center">
-              <div className="w-10 h-10 rounded-full bg-gray-200"></div>
-              <div className="w-40 h-6 bg-gray-200"></div>
-            </li>
-          ))}
-      </ul>
+      <article className="bg-white h-[320px] px-6 pt-5 flex flex-col gap-y-3 rounded-lg shadow-sm">
+        <div className="w-40 h-6 m-auto bg-gray-200" />
+        <ul className="grid grid-cols-1 grid-rows-5 gap-y-2">
+          {Array(5)
+            .fill(null)
+            .map((_, index) => (
+              <li key={index} className="flex gap-x-4 items-center">
+                <div className="w-10 h-10 rounded-full bg-gray-200"></div>
+                <div className="w-40 h-6 bg-gray-200"></div>
+              </li>
+            ))}
+        </ul>
+      </article>
     );
 
   return (
